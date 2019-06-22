fe0a6684cfe1c3296b555632846aea506bbe983a
gugu <lar542@naver.com>
2019-06-22 23:31:04 +0900

    Django list

--- third/views.py
+++ third/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from . models import Restaurant
 
 # Create your views here.
 def list(request):
     context = {
-
+        "restaurants": Restaurant.objects.all()
     }
     return render(request, 'third/list.html', context)
+
